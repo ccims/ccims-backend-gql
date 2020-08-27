ca17f2bd2c0cb75182e81f7a5e46a0295b07cb2b
ModellbahnFreak <freak.mdellbahn@gmail.com>
2020-08-27 23:55:53 +0200

    Table for ims systems

--- database/ims.sql
+++ database/ims.sql
@@ -1,3 +1,12 @@
+CREATE TABLE ims_systems
+(
+    id id NOT NULL,
+    type ims_type NOT NULL,
+    endpoint text NOT NULL,
+    connection_data json NOT NULL,
+    PRIMARY KEY (id)
+);
+
 CREATE TABLE user_ims_credentials
 (
     id id NOT NULL,
