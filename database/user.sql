b0398b73c199c83c176ebfdd0b42f06fdc3059dc
ModellbahnFreak <freak.mdellbahn@gmail.com>
2020-08-28 00:52:11 +0200

    Adapting names to naming convention

--- database/user.sql
+++ database/user.sql
@@ -1,16 +1,16 @@
 CREATE TABLE users
 (
-    -- Inherited from table public.node: id id NOT NULL,
     username character varying(100) NOT NULL,
     displayname character varying(200) NOT NULL,
     pw_hash character varying(200) NOT NULL,
-    email character varying(320)
-) INHERITS (public.node);
+    email character varying(320),
+    permissions bytea NOT NULL,
+) INHERITS (node);
 
-CREATE TABLE user_projects
+CREATE TABLE relation_user_project
 (
     user_id id NOT NULL,
     project_id id NOT NULL,
-    added_date date NOT NULL,
+    added_date timestamp NOT NULL,
     PRIMARY KEY (user_id, project_id)
 );
