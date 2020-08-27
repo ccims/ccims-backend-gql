8205153242b75a354a6f9515c80f8c154b86800d
ModellbahnFreak <freak.mdellbahn@gmail.com>
2020-08-27 23:55:25 +0200

    Added ims_type enum

--- database/common.sql
+++ database/common.sql
@@ -2,9 +2,8 @@ CREATE DOMAIN id AS BIGINT;
 
 
 CREATE TYPE issue_type AS ENUM ('Bug', 'FeatureRequest', 'General');
-
 CREATE TYPE priority AS ENUM ('Low', 'Medium', 'High');
-
+CREATE TYPE ims_type AS ENUM ('GitHub', 'GitLab', 'Jira', 'Redmine');
 
 CREATE TABLE Node (
     id id PRIMARY KEY
