2bd4e221cb260a1ed6b351ce20a563c5532af84a
ModellbahnFreak <freak.mdellbahn@gmail.com>
2020-08-27 22:24:19 +0200

    Added script for combining database files into one

---
+++ scripts/database.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const path = require("path");
+
+var databaseScript = "";
+
+var files = ["reset.sql", "common.sql", "issue.sql"];
+files.filter(name => name.match(/.*\.sql/i)).forEach(name => {
+    databaseScript += fs.readFileSync(path.join(__dirname, "..", "database", name), { encoding: "utf-8" });
+});
+
+var outFileName = "";
+outFileName = process.argv.filter(param => param.match(/.*.sql/i))[0] || "";
+if (outFileName.length > 0 && outFileName != "-") {
+    fs.writeFileSync(path.join(outFileName), databaseScript);
+} else {
+    process.stdout.write(databaseScript);
+}
