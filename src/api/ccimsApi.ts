83f4af2c4699380b090d647ffbe40b0e761a3739
ModellbahnFreak <freak.mdellbahn@gmail.com>
2020-09-01 02:18:58 +0200

    Added classes for syncService and api base

---
+++ src/api/ccimsApi.ts
@@ -0,0 +1,10 @@
+export class CCIMSApi {
+
+    public constructor() {
+        console.log("Initializing api");
+    }
+
+    public start(): void {
+        console.log("Starting api server");
+    }
+}
