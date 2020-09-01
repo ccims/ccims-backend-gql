3aaf63c09aafd8ac00d0c9ddfa8d2bfe20c60865
ModellbahnFreak <freak.mdellbahn@gmail.com>
2020-09-01 03:40:06 +0200

    Added basic express server

--- src/api/ccimsApi.ts
+++ src/api/ccimsApi.ts
@@ -1,10 +1,20 @@
+import express from "express";
+import * as core from "express-serve-static-core";
+import { config } from "../config";
+
 export class CCIMSApi {
+
+    private server: core.Express;
 
     public constructor() {
         console.log("Initializing api");
+        this.server = express();
     }
 
     public start(): void {
         console.log("Starting api server");
+        this.server.listen(config.api.port, () => {
+            console.log("API server started.");
+        });
     }
 }
