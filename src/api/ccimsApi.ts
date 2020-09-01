5dbc3c436f24840a4a9744e41d07b0b7792065bc
ModellbahnFreak <freak.mdellbahn@gmail.com>
2020-09-01 13:29:12 +0200

    Added documentation to CCIMSApi and using login handler

--- src/api/ccimsApi.ts
+++ src/api/ccimsApi.ts
@@ -1,20 +1,63 @@
 import express from "express";
 import * as core from "express-serve-static-core";
 import { config } from "../config";
+import { loginHandler } from "./auth/login";
+import bodyParser from "body-parser";
 
+/**
+ * The main class for the API part of ccims. It is the enty-point for the GraphQL-API to the frontend and other clients
+ */
 export class CCIMSApi {
 
+    /**
+     * The Instance of the express server used by this instance of the API
+     */
     private server: core.Express;
 
-    public constructor() {
+    /**
+     * The port which the API server is supposed to listen on for login and API requests
+     */
+    private port: number;
+
+    /**
+     * The IP-Address of the network interface the server will be bound to; empty or 0.0.0.0 for all interfaces
+     */
+    private hostIface: string;
+
+    /**
+     * Constructs the API server but doesn't start it yet. This will only prepare the server, start it using `start()`
+     * 
+     * @param port Port on which the API and login server will listen.\
+     * If `undefined`, value from config will be used.
+     * 
+     * @param hostIface The IP address of the network interface to which the server will be bound.\
+     *  If empty, or `undefined`, the value in the config will be used.\
+     * If 0.0.0.0 server will be bound to all availale interfaces.
+     */
+    public constructor(port?: number, hostIface?: string) {
         console.log("Initializing api");
+        this.port = port || config.api.port;
+        this.hostIface = hostIface || config.api.hostIface || "0.0.0.0";
+        if (this.hostIface.length == 0) {
+            this.hostIface = "0.0.0.0";
+        }
         this.server = express();
     }
 
+    /**
+     * Starts the API server
+     * 
+     * When calling this method the server will be bound to the interfaces and the port 
+     * specified in the constructor and start accepting incoming requests.\
+     * The server listenes for the following types of requests:
+     * - POST to `/login` for authenticating a user and requesting a JWT for the session
+     * - POST to `/api` with a valid JWT in the `Authorization` header field
+     */
     public start(): void {
         console.log("Starting api server");
-        this.server.listen(config.api.port, () => {
+        this.server.listen(this.port, this.hostIface, () => {
             console.log("API server started.");
         });
+        this.server.post("/login", bodyParser.json(), loginHandler());
     }
 }
