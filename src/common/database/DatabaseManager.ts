775546446066c59fedb8f8337925ca67c363519d
nk-coding <niklask.coding@gmail.com>
2020-09-01 02:09:54 +0200

    added idGenerator, added methods for get and add nodes on the DatabaseManager

--- src/common/database/DatabaseManager.ts
+++ src/common/database/DatabaseManager.ts
@@ -1,3 +1,31 @@
+import { CCIMSNode } from "../nodes/CCIMSNode";
+import { IdGenerator } from "../util/IdGenerator";
+
 export class DatabaseManager {
+    nodes: Map<string, CCIMSNode> = new Map<string, CCIMSNode>();
+
+    public readonly idGenerator: IdGenerator;
+
+    public constructor (idGenerator: IdGenerator) {
+        this.idGenerator = idGenerator;
+    }
+
+    /**
+     * Adds the node to the internal node dictionary
+     * This must be used to add new nodes, but also to register loaded nodes
+     * Caution: if a node with the same id is already registered, it is overridden
+     * @param node the node to register
+     */
+    public addNode(node: CCIMSNode): void {
+        this.nodes.set(node.id, node);
+    }
+
+    /**
+     * gets the node with the associated id or undefined if not found
+     * @param id the id for the node to get
+     */
+    public getNode(id: string): CCIMSNode | undefined {
+        return this.nodes.get(id);
+    }
     
 }
