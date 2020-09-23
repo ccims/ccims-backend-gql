48cec76fe4333a7e77142f898bc6967b848a414b
ModellbahnFreak <freak.mdellbahn@gmail.com>
2020-09-24 01:04:27 +0200

    renameIssueTitle mutation

--- src/api/resolvers/mutations/issue/renameIssueTitle.ts
+++ src/api/resolvers/mutations/issue/renameIssueTitle.ts
@@ -2,20 +2,24 @@ import { GraphQLFieldConfig } from "graphql";
 import { ResolverContext } from "../../../ResolverContext";
 import GraphQLRenameIssueTitlePayload from "../../types/mutations/payloads/issue/GraphQLRenameIssueTitlePayload";
 import GraphQLRenameIssueTitleInput from "../../types/mutations/inputs/issue/GraphQLRenameIssueTitleInput";
+import timelineMutation from "./timelineMutation";
+import PreconditionCheck from "../../utils/PreconditionCheck";
 
-let renameIssueTitle: GraphQLFieldConfig<any, ResolverContext> | undefined;
-export default () => {
-    if (renameIssueTitle === undefined) {
-        renameIssueTitle = {
-            type: GraphQLRenameIssueTitlePayload,
-            description: "Change the title (rename) an issue",
-            args: {
-                input: {
-                    type: GraphQLRenameIssueTitleInput,
-                    description: "The data for the mutation"
-                }
+function renameIssueTitle(): GraphQLFieldConfig<any, ResolverContext> {
+    const base = timelineMutation(GraphQLRenameIssueTitlePayload, GraphQLRenameIssueTitleInput, "Change the title (rename) an issue");
+    return {
+        ...base,
+        resolve: async (src, args, context, info) => {
+            const { input, cmd } = base.initTimelineMutation(args, context);
+            const title = PreconditionCheck.checkString(input, "newTitle", 256);
+            if (title.length <= 0) {
+                throw new Error("The title text can't be empty");
             }
-        };
+            const issue = await base.getIssue(cmd, context, (perm, issueObj) => perm.componentAdmin || perm.moderate || (perm.editIssues && issueObj.createdByProperty.getId() === context.user.id));
+            const event = await issue.changeTitle(title, new Date(), context.user);
+            await context.dbManager.save();
+            return base.createResult(args, issue, event, {});
+        }
     }
-    return renameIssueTitle;
-};
+}
+export default renameIssueTitle;
