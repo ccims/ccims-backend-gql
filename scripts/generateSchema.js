844dba96d74092198d3f68394a9b615574b289df
ModellbahnFreak <freak.mdellbahn@gmail.com>
2020-09-24 12:38:47 +0200

    Added public schema generation to generateSchema.js

--- scripts/generateSchema.js
+++ scripts/generateSchema.js
@@ -2,12 +2,21 @@ const fs = require("fs");
 const path = require("path");
 const { printSchema } = require("graphql");
 const ccimsSchema = require("../out/api/resolvers/CCIMSSchema");
+const ccimsPublicSchema = require("../out/api/publicResolvers/ccimsPublicSchema");
 
-var gqlSchema = printSchema(ccimsSchema.default);
+const printPublicSchema = process.argv.some(param => !!param.match(/^-(-)?p(ublic)?$/i));
+let gqlSchema = "";
+if (printPublicSchema) {
+    gqlSchema = printSchema(ccimsPublicSchema.default);
+} else {
+    gqlSchema = printSchema(ccimsSchema.default);
+}
 
 var outFileName = "";
-outFileName = process.argv.filter(param => param.match(/.*.graphql/i))[0] || "";
-if (outFileName.length > 0 && outFileName != "-") {
+outFileName = process.argv.filter(param => !!param.match(/.g(raph)?ql$/i))[0] || "";
+console.log(process.argv);
+console.log(outFileName);
+if (outFileName.length > 0 && outFileName !== "-") {
     fs.writeFileSync(path.join(outFileName), gqlSchema);
 } else {
     process.stdout.write(gqlSchema);
