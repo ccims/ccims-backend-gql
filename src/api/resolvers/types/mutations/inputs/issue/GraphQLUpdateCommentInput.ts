bc34359e26b6276aad5884de4977767ad7d41816
nk-coding <53957498+nk-coding@users.noreply.github.com>
2020-11-20 19:43:01 +0100

    added update comment mutation to change comment body

---
+++ src/api/resolvers/types/mutations/inputs/issue/GraphQLUpdateCommentInput.ts
@@ -0,0 +1,22 @@
+import { GraphQLInputFieldConfig, GraphQLInputObjectType, GraphQLInputObjectTypeConfig, GraphQLString, GraphQLNonNull, GraphQLID } from "graphql";
+
+const updateCommentInputConfig: GraphQLInputObjectTypeConfig = {
+    name: "UpdateCommentInput",
+    description: "The inputs for the updateComment",
+    fields: () => ({
+        clientMutationID: {
+            type: GraphQLString,
+            description: "An arbitraty string to return together with the mutation result"
+        },
+        comment: {
+            type: GraphQLNonNull(GraphQLID),
+            description: "The ID of the comment to update"
+        },
+        body: {
+            type: GraphQLNonNull(GraphQLString),
+            description: "The body text of the comment to be updated.\n\nMax. 65536 characters."
+        }
+    })
+};
+const GraphQLUpdateCommentInput = new GraphQLInputObjectType(updateCommentInputConfig);
+export default GraphQLUpdateCommentInput;
