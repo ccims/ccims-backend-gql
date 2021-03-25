0cc8c472e407b2fe78fbc57a760c48140c1fb8b4
nk-coding <niklask.coding@gmail.com>
2021-03-25 09:08:47 +0100

    removed dummy test

--- test/syncService/syncServiceTest.ts
+++
@@ -1,8 +0,0 @@
-import { SyncService } from "../../src/syncService/syncService";
-
-describe('syncService', function () {
-    it('testTimer', function () {
-        var service = new SyncService();
-
-    });
-});
