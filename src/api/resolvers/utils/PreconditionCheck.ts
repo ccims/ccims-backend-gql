b7897d939316c661efffe1ebe6d732f5e6a3c547
nk-coding <niklask.coding@gmail.com>
2021-04-16 23:44:25 +0200

    refactored precondition check

--- src/api/resolvers/utils/PreconditionCheck.ts
+++ src/api/resolvers/utils/PreconditionCheck.ts
@@ -1,13 +1,6 @@
 export default class PreconditionCheck {
-    public static checkString(args: any, property: string, maxLength?: number): string {
-        const checked = PreconditionCheck.checkNullableString(args, property, maxLength);
-        if (typeof checked !== "string") {
-            throw new Error(`The ${property} input must be set`);
-        }
-        return checked;
-    }
 
-    public static checkNullableString(args: any, property: string, maxLength?: number): string | undefined {
+    public static checkString(args: any, property: string, maxLength?: number): string {
         if (typeof args[property] === "string") {
             if (maxLength && maxLength > 0) {
                 if (args[property].length > maxLength) {
@@ -15,53 +8,37 @@ export default class PreconditionCheck {
                 }
             }
             return args[property];
-        } else if (typeof args[property] === "undefined") {
-            return undefined;
         } else {
             throw new Error(`The ${property} input must be, if set, a string`);
         }
     }
 
+    public static checkNullableString(args: any, property: string, maxLength?: number): string | undefined {
+        return PreconditionCheck.checkNullable(args, property, () => PreconditionCheck.checkString(args, property, maxLength));
+    }
+
     public static checkStringList(args: any, property: string, maxStrLength?: number, maxArrayLength?: number): string[] {
-        const checked = PreconditionCheck.checkNullableStringList(args, property, maxStrLength, maxArrayLength);
-        if (typeof checked === "undefined") {
-            throw new Error(`The ${property} input must be set`);
+        if (!Array.isArray(args[property])) {
+            throw new Error(`The ${property} input must be, if set, a valid string array`);
         }
-        return checked;
+        const list: any[] = args[property];
+        if (maxArrayLength && list.length > maxArrayLength) {
+            throw new Error(`The ${property} input array cant have more than ${maxArrayLength} elements`);
+        }
+        if (list.some(obj => typeof obj !== "string")) {
+            throw new Error(`The ${property} input must be a valid list of strings`);
+        }
+        if (maxStrLength && list.some(str => (str as string).length > maxStrLength)) {
+            throw new Error(`The strings in the ${property} input array must have a length of max. ${maxStrLength} characters`);
+        }
+        return list;
     }
 
     public static checkNullableStringList(args: any, property: string, maxStrLength?: number, maxArrayLength?: number): string[] | undefined {
-        if (typeof args[property] === "undefined") {
-            return undefined;
-        } else {
-            if (!Array.isArray(args[property])) {
-                throw new Error(`The ${property} input must be, if set, a valid string array`);
-            }
-            const list: any[] = args[property];
-            if (maxArrayLength && list.length > maxArrayLength) {
-                throw new Error(`The ${property} input array cant have more than ${maxArrayLength} elements`);
-            }
-            if (list.some(obj => typeof obj !== "string")) {
-                throw new Error(`The ${property} input must be a valid list of strings`);
-            }
-            if (maxStrLength && list.some(str => (str as string).length > maxStrLength)) {
-                throw new Error(`The strings in the ${property} input array must have a length of max. ${maxStrLength} characters`);
-            }
-            return list;
-        }
+        return PreconditionCheck.checkNullable(args, property, () => this.checkStringList(args, property, maxStrLength, maxArrayLength));
     }
 
-    public static checkNonNull(args: any, property: string): any {
-        if (typeof args[property] === "undefined" || args[property] === null) {
-            throw new Error(`The ${property} input must be set and can't be undefined`);
-        }
-        return args[property];
-    }
-
-    public static checkNullableEnum<TEnum>(args: any, property: string, enumType: any): TEnum | undefined {
-        if (typeof args[property] === "undefined") {
-            return undefined;
-        }
+    public static checkEnum<TEnum>(args: any, property: string, enumType: any): TEnum {
         if (!Object.values(enumType).includes(args[property])) {
             const value = enumType[args[property]];
             if (value) {
@@ -72,18 +49,12 @@ export default class PreconditionCheck {
         return args[property];
     }
 
-    public static checkEnum<TEnum>(args: any, property: string, enumType: any): TEnum {
-        const value = this.checkNullableEnum<TEnum>(args, property, enumType);
-        if (typeof value === "undefined") {
-            throw new Error(`The ${property} input must be set`);
-        }
-        return value;
+    public static checkNullableEnum<TEnum>(args: any, property: string, enumType: any): TEnum | undefined {
+        return PreconditionCheck.checkNullable(args, property, () => PreconditionCheck.checkEnum(args, property, enumType));
     }
 
-    public static checkNullableDate(args: any, property: string, earliest?: Date, latest?: Date): Date | undefined {
-        if (typeof args[property] === "undefined") {
-            return undefined;
-        } else if (typeof args[property] === "object" && args[property] instanceof Date) {
+    public static checkDate(args: any, property: string, earliest?: Date, latest?: Date): Date {
+        if (typeof args[property] === "object" && args[property] instanceof Date) {
             const date: Date = args[property];
             if (earliest && earliest > date) {
                 throw new Error(`The ${property} input must be a date after ${earliest.toISOString()}`);
@@ -98,18 +69,12 @@ export default class PreconditionCheck {
         throw new Error(`The type given for the ${property} input must be a date after ${new Date(0).toISOString()} or undefined`);
     }
 
-    public static checkDate(args: any, property: string, earliest?: Date, latest?: Date): Date {
-        const date = this.checkNullableDate(args, property, earliest, latest);
-        if (typeof date === "undefined") {
-            throw new Error(`The ${property} input must be a date and can't be \`undefined\``);
-        }
-        return date;
+    public static checkNullableDate(args: any, property: string, earliest?: Date, latest?: Date): Date | undefined {
+        return PreconditionCheck.checkNullable(args, property, () => PreconditionCheck.checkDate(args, property, earliest, latest));
     }
 
-    public static checkNullableTimespan(args: any, property: string, maxTimespan?: number): number | undefined {
-        if (typeof args[property] === "undefined") {
-            return undefined;
-        } else if (typeof args[property] === "number") {
+    public static checkTimespan(args: any, property: string, maxTimespan?: number): number {
+        if (typeof args[property] === "number") {
             const timespan: number = args[property];
             if (!Number.isSafeInteger(timespan)) {
                 throw new Error(`The ${property} input must be a integer number`);
@@ -117,15 +82,66 @@ export default class PreconditionCheck {
             if (!isFinite(timespan) || timespan < 0) {
                 throw new Error(`The ${property} input must be a finite number >=0`);
             }
+            if (maxTimespan != undefined && timespan > maxTimespan) {
+                throw new Error(`The ${property} must be <= ${maxTimespan}`);
+            }
+            return timespan;
         }
         throw new Error(`The ${property} input must be a valid timepan (whole number in milliseconds) greater than 0 or \`undefined\``);
     }
 
-    public static checkTimespan(args: any, property: string, maxTimespan?: number): number {
-        const timespan = this.checkTimespan(args, property, maxTimespan);
-        if (typeof timespan === "undefined") {
-            throw new Error(`The ${property} input must be a non null timespan (whole number in milliseconds)`);
-        }
-        return timespan;
+    public static checkNullableTimespan(args: any, property: string, maxTimespan?: number): number | undefined {
+        return PreconditionCheck.checkNullable(args, property, () => PreconditionCheck.checkTimespan(args, property, maxTimespan));
     }
+
+    public static checkInteger(args: any, property: string, minInt?: number, maxInt?: number): number {
+        if (typeof args[property] === "number") {
+            const integer: number = args[property];
+            if (!Number.isSafeInteger(integer)) {
+                throw new Error(`The ${property} input must be a integer number`);
+            }
+            if (minInt != undefined && integer < minInt) {
+                throw new Error(`The ${property} input must be >= ${minInt}`);
+            }
+            if (maxInt != undefined && integer > maxInt) {
+                throw new Error(`The ${property} input must be <= ${maxInt}`);
+            }
+            return integer;
+        }
+        throw new Error(`The ${property} input must be a valid integer`);
+    }
+
+    public static checkNullableInteger(args: any, property: string, minInt?: number, maxInt?: number): number | undefined {
+        return PreconditionCheck.checkNullable(args, property, () => PreconditionCheck.checkInteger(args, property, minInt, maxInt));
+    }
+
+    /**
+     * Checks a parameter
+     * If null or undefined is provided undefined is returned
+     * Otherwise checkValue(args, property) is returned
+     */
+    public static checkNullable<T>(args: any, property: string, checkValue: () => T): T | undefined {
+        if (args[property] == undefined) {
+            return undefined;
+        } else {
+            return checkValue();
+        }
+    }
+
+    /**
+     * Checks a parameter
+     * If null is provided, null is returned
+     * If undefined is provided, undefined is returned
+     * Otherwise checkValue(args, property) is returned
+     */
+    public static checkNullableKeepNull<T>(args: any, property: string, checkValue: () => T): T | undefined | null {
+        if (args[property] === undefined) {
+            return undefined;
+        } else if (args[property] === null) {
+            return null;
+        } else {
+            return checkValue();
+        }
+    }
+
 }
